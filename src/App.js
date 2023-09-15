60a50365ec5f86f41d9738d1b162b31da337943c
avk <avkovaley@gmail.com>
2023-09-15 17:45:58 +0300

    first commit react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css"
+
+
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Projects from "./components/pages/Projects";
+// import Contacts from "./components/pages/Contacts";
+// import Home from "./components/pages/Home";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      {/* <Home /> */}
+      <Projects />
+      {/* <Contacts /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/helpers/projectsList.js
@@ -0,0 +1,25 @@
+
+import project01 from "./../../img/projects/01.jpg";
+import project01Big from "./../../img/projects/01-big.jpg";
+
+
+const projects = [
+    {
+
+        title: "Gaming streaming portal",
+        skills: "NodeJS, MySQL, MongoDB, PHP, Laravel",
+        img: project01,
+        gitHubLink: "https://github.com/",
+
+
+    },
+
+    {
+
+        title: "Gaming streaming portal",
+        skills: "NodeJS, MySQL, MongoDB, PHP, Laravel",
+        img: project01,
+        gitHubLink: "https://github.com/",
+
+    }
+]
